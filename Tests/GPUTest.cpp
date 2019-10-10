6296f0c578f1940189481f290ee4cef309ecd7b6
Bence Nagy <bence657@gmail.com>
2019-10-10 20:10:10 +0200

    Created new test

---
+++ Tests/GPUTest.cpp
@@ -0,0 +1,29 @@
+#include <stdlib.h>
+#include "QPULib.h"
+
+
+int data[4][4] = {{1, 2, 3, 4}, {5, 6, 7, 8}, {9, 10, 11, 12}, {13, 14, 15, 16}};
+
+void vector_add(Ptr<Int> a, Ptr<Int> b, Ptr<Int> r) {
+	*r = *a + *b;
+}
+
+int main() {
+  // Construct kernel
+  auto k = compile(vector_add);
+
+  // Allocate and initialise arrays shared between ARM and GPU
+  SharedArray<int> a(16), b(16), r(16);
+  srand(0);
+  for (int i = 0; i < 16; i++) {
+    a[i] = 100 + (rand() % 100);
+    b[i] = 100 + (rand() % 100);
+  }
+
+  // Invoke the kernel and display the result
+  k(&a, &b, &r);
+  for (int i = 0; i < 16; i++)
+    printf("%i + %i = %i\n", a[i], b[i], r[i]);
+  
+  return 0;
+}
