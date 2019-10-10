4eb96d048714a5a713c8a1d39d05be7a04143244
Bence Nagy <bence657@gmail.com>
2019-10-10 20:12:18 +0200

    Changed compiler and made changes to gpu test

--- Tests/GPUTest.cpp
+++ Tests/GPUTest.cpp
@@ -14,14 +14,15 @@ int main() {
 
   // Allocate and initialise arrays shared between ARM and GPU
   SharedArray<int> a(16), b(16), r(16);
-  srand(0);
+  
   for (int i = 0; i < 16; i++) {
-    a[i] = 100 + (rand() % 100);
-    b[i] = 100 + (rand() % 100);
+    a[i] = i + 1;
+    b[i] = i + 1;
   }
 
   // Invoke the kernel and display the result
   k(&a, &b, &r);
+  
   for (int i = 0; i < 16; i++)
     printf("%i + %i = %i\n", a[i], b[i], r[i]);
   
